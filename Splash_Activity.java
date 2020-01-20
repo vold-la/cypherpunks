a6d065c71a239e0ac7382c1c3bb4a709431ff8f7
vold-la <42201292+vold-la@users.noreply.github.com>
2020-01-20 16:53:01 +0530

    Add files via upload

---
+++ Splash_Activity.java
@@ -0,0 +1,44 @@
+package com.noori.bbbp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Thread timer = new Thread() {
+            public void run(){
+                try{
+                    sleep(10000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(Splash_Activity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+
+}
+
+
+
+
