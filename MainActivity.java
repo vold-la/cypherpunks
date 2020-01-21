19da818d4f177eb6d135e5ba6f1a3c7fdc76444a
vold-la <42201292+vold-la@users.noreply.github.com>
2020-01-21 16:38:10 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -4,8 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void level(View view){
+        ImageView level = (ImageView) view;
+        int  ilevel = Integer.parseInt(level.getTag().toString());
+        switch (ilevel){
+            case 0:
+                Intent intent = new Intent(this,);
+                startActivity(intent);
+             break;
+
+
+
+
+        }
+
+
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ level1.java
@@ -0,0 +1,13 @@
+package com.noori.bbbp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level1);
+    }
+}
